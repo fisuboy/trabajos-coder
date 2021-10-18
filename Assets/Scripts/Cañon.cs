1e0796b039e6b6bab5302f03513da6e264e762c8
Ignacio Luna <ignacio.luna@live.com>
2021-10-17 22:06:45 -0300

    desafio terminado

--- Assets/Scripts/Cañon.cs
+++ Assets/Scripts/Cañon.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnBala : MonoBehaviour
+public class Cañon : MonoBehaviour
 {
     public GameObject bala;
     
